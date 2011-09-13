860785b51affe0f1215bb56d7564863279815e09
JTBartel <perfectcombo555@aol.com>
2011-09-13 13:43:47 -0400

    initial cammit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeaponsApp::Application.config.secret_token = '916a6bba845549549f7e8ce8a681c1ffd91f0da8c8df88c342168ac638cdd4c486504db025424614c6f23f74b3d6b8f36e7f7ceac50dbaf685a41958c28dd372'
