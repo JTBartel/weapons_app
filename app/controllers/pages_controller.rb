17eaae872ac39797921e7dc0701622201926d554
JTBartel <perfectcombo555@aol.com>
2011-09-27 09:19:26 -0400

    made app html

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,21 +1,30 @@
 class PagesController < ApplicationController
   def home
-    
+    @title = "Home"  
   end
 
   def contact
+    @title = "Contact"
   end
 
   def help
+    @title = "Help"
   end
 
   def advertising
+    @title = "Advertising"
   end
 
   def privacy
+    @title = "Privacy"
   end
 
   def testimonies
+    @title = "Testimonies"
+  end
+  
+  def about
+    @title = "About"
   end
 
 end
