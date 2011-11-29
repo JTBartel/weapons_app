14043ca8517fbb032c533895b4caf9d5d2d3a8f0
JTBartel <perfectcombo555@aol.com>
2011-11-29 11:31:53 -0500

    frendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
   def new
     @title = "Sign up"
     @user = User.new
@@ -17,7 +19,21 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+    @title = "Edit user"
   end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+  
 
   def index
   end
@@ -26,5 +42,13 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @title = @user.name    
   end
+  private
 
+    def authenticate
+      deny_access unless signed_in?
+    end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
