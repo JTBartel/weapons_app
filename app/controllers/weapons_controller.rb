e30abb17db4394993e60c80dd642f97e6dc237b4
JTBartel <perfectcombo555@aol.com>
2011-12-13 12:31:45 -0500

    weapon controller

--- app/controllers/weapons_controller.rb
+++ app/controllers/weapons_controller.rb
@@ -1,29 +1,54 @@
 class WeaponsController < ApplicationController
-  before_filter :authenticate, :only => [:create, :destroy]
+  before_filter :authenticate, :only => [:create, :update, :destroy]
     
   def new
+    @title = "Add Weapon"
+    @user = current_user
+    @weapon = Weapon.new
   end
   
   def create
+    @user = current_user
     @weapon  = current_user.weapons.build(params[:weapon])
     if @weapon.save
-      flash[:success] = "Weapon created!"
-      redirect_to root_path
+      flash[:success] = "Weapon Created!"
+      redirect_to weapons_path
     else
-      render 'pages/home'
+      render 'new'
     end
   end
 
   def show
+    @title = "Weapon Page"
+    @weapon = Weapon.find(params[:id])
   end
 
   def edit
+    @title = "Edit Weapon"
+    @user = current_user
+    @weapon = Weapon.find(params[:id])
+  end
+  
+  def update
+    @user = current_user
+    @weapon  = Weapon.update(params[:id], params[:weapon])
+    if @weapon.update_attributes(params[:weapon])
+      flash[:success] = "Weapon Updated!"
+      render 'show'
+    else
+      render 'edit'
+    end
   end
 
   def index
     @title = "Weapon Inventory"
     @user = current_user
     @weapons = Weapon.paginate(:per_page => 10, :page => params[:page])
+  end
+  
+  def destroy
+    @weapon = Weapon.find(params[:id])
+    redirect_to weapons_path, :flash => { :success => "weapon deleted"}
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ WeaponsApp::Application.routes.draw do
     resources :weapons
   end
   
+  resources :weapons
+  
   resources :sessions, :only => [:new, :create, :destroy] 
   match '/signup'     ,         :to => 'users#new'
   match '/signin'     ,         :to => 'sessions#new'
